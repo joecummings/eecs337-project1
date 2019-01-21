efcfbb2d0a99a21e1c5d7eec76cb0f5439c6040d
Joe Cummings <jrcummings27@gmail.com>
2019-01-21 16:23:40 -0600

    asdf

--- data.py
+++ data.py
@@ -7,23 +7,24 @@ fileName = "gg2013.json"
 with open(fileName) as data_file:
     rawData = json.load(data_file)
 
+
 def uniConvert(uniData):
-    return unicodedata.normalize('NFKD', uniData).encode('ascii','ignore')
+    return unicodedata.normalize('NFKD', uniData).encode('ascii', 'ignore')
+
 
 def mostOften(lst):
-     return max(lst,key=lst.count)
+    return max(lst, key=lst.count)
 
 
-#Note everything is in unicode
+# Note everything is in unicode
 # print rawData[0]
 # print rawData[0].keys()
 # print rawData[0]['text']
 
-hostTweets = [ uniConvert(tweet['text']) for tweet in rawData if 'host' in tweet['text']]
+hostTweets = [uniConvert(tweet['text']) for tweet in rawData if 'host' in tweet['text']]
 capitalWordsHostTweets = [re.findall('([A-Z][a-z]+)', tweet) for tweet in hostTweets]
 hostCapitalWords = [word for words in capitalWordsHostTweets for word in words]
 print mostOften(hostCapitalWords)
-
 
 
 # namesOfHostTweets = [ re.findall('([A-Z][\w-]*(?:\s+[A-Z][\w-]*)+)', tweet) for tweet in hostTweets]
@@ -32,10 +33,3 @@ print mostOften(hostCapitalWords)
 # result_dict = dict( [ (i, src.count(i)) for i in set(src) ] )
 # print result_dict
 # print mostOften(hostNames)
-
-# 
-
-
-
-
-
