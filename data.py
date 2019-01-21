201bf903c3ab5cc56ff541d74f46dfbc971789bc
danimal4580 <danielknight2019@u.northwestern.edu>
2019-01-21 14:17:58 -0600

    set up a few helpers

---
+++ data.py
@@ -0,0 +1,41 @@
+import json
+import unicodedata
+import regex as re
+
+fileName = "gg2013.json"
+
+with open(fileName) as data_file:
+    rawData = json.load(data_file)
+
+def uniConvert(uniData):
+    return unicodedata.normalize('NFKD', uniData).encode('ascii','ignore')
+
+def mostOften(lst):
+     return max(lst,key=lst.count)
+
+
+#Note everything is in unicode
+# print rawData[0]
+# print rawData[0].keys()
+# print rawData[0]['text']
+
+hostTweets = [ uniConvert(tweet['text']) for tweet in rawData if 'host' in tweet['text']]
+capitalWordsHostTweets = [re.findall('([A-Z][a-z]+)', tweet) for tweet in hostTweets]
+hostCapitalWords = [word for words in capitalWordsHostTweets for word in words]
+print mostOften(hostCapitalWords)
+
+
+
+# namesOfHostTweets = [ re.findall('([A-Z][\w-]*(?:\s+[A-Z][\w-]*)+)', tweet) for tweet in hostTweets]
+# hostNames = [name for names in namesOfHostTweets for name in names]
+# src = hostNames
+# result_dict = dict( [ (i, src.count(i)) for i in set(src) ] )
+# print result_dict
+# print mostOften(hostNames)
+
+# 
+
+
+
+
+
